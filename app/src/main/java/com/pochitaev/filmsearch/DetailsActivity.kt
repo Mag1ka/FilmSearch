279a6a6a6814dbfae7dba829cc51ab9b4b217d3e
Dima <70973126+Mag1ka@users.noreply.github.com>
2021-02-09 08:31:41 +0300

    27 Finish (SnackBar add)

--- app/src/main/java/com/pochitaev/filmsearch/DetailsActivity.kt
+++ app/src/main/java/com/pochitaev/filmsearch/DetailsActivity.kt
@@ -2,6 +2,8 @@ package com.pochitaev.filmsearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_details.*
 
 class DetailsActivity : AppCompatActivity() {
@@ -9,10 +11,42 @@ class DetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
 
+
+        details_toolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.details_favorites-> {
+                    Snackbar.make(details_description, "Избранные", Snackbar.LENGTH_SHORT)
+                        .show()
+                    true
+
+                }
+                R.id.details_watch_later-> {
+                    Snackbar.make(details_description, "Посмотреть позже", Snackbar.LENGTH_SHORT)
+
+                        .show()
+                    true
+
+                }
+                R.id.details_share-> {
+                    Snackbar.make(details_description, "Поделиться", Snackbar.LENGTH_SHORT)
+
+                        .show()
+                    true
+
+                }
+
+                else -> false
+            }
+
+        }
+
+
+
         val film = intent.extras?.get("film") as Film
 
         details_toolbar.title = film.title
         details_poster.setImageResource(film.poster)
         details_description.text = film.description
     }
+
 }
