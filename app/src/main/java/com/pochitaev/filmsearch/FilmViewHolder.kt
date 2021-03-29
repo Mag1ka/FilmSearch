c411b613756ded4e31187468c26cb5e6ea45ac0c
Dima <70973126+Mag1ka@users.noreply.github.com>
2021-03-29 14:56:13 +0300

    CustomView add

--- app/src/main/java/com/pochitaev/filmsearch/FilmViewHolder.kt
+++ app/src/main/java/com/pochitaev/filmsearch/FilmViewHolder.kt
@@ -3,7 +3,6 @@ package com.pochitaev.filmsearch
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.pochitaev.filmsearch.Film
 import kotlinx.android.synthetic.main.film_item.view.*
 
 //В конструктор класс передается layout, который мы создали(film_item.xml)
@@ -12,7 +11,7 @@ class FilmViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val title = itemView.title
     private val poster = itemView.poster
     private val description = itemView.description
-
+    private val ratingDonut = itemView.rating_donut
     //В этом методе кладем данные из Film в наши View
     fun bind(film: Film) {
         //Устанавливаем заголовок
@@ -29,5 +28,7 @@ class FilmViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
             .centerCrop()
             //Указываем ImageView, куда будем загружать изображение
             .into(poster)
+        //Устанавливаем рэйтинг
+        ratingDonut.setProgress((film.rating * 10).toInt())
     }
 }
